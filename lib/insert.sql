e8b59d507c38561c8355e1aa5b1a296b09f3fd11
Isaac <isaacusungio@gmail.com>
2023-10-24 10:47:06 +0300

    Done

---
+++ lib/insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES 
+    ("Mr. Chocolate", 10, "M", "gold", "fierce", TRUE),
+    ("Rowdy", 5, "M", "black-tan", "loving", TRUE),
+    ("Tabitha", 3, "F", "Black", "mama-bear", TRUE),
+    ("Sergeant Brown", 20, "M", "brown", "smart", FALSE),
+    ("Melissa", 1, "F", "brown", "cute", TRUE),
+    ("Grinch", 7, "M", "black", "defender", FALSE),
+    ("Wendy", 18, "F", "white", "leader", TRUE),
+    (NULL, 17, "M", "tan", "loyal", TRUE);
+
+
+
